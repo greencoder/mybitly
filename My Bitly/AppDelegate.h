75f851ee03c43830235065da5796095e5b908d40
Scott Newman <snewman18@gmail.com>
2015-11-17 18:37:09 -0700

    Initial Commit

---
+++ My Bitly/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  My Bitly
+//
+//  Created by Scott Newman on 11/17/15.
+//  Copyright © 2015 Newman Creative. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ My Bitly/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  My Bitly
+//
+//  Created by Scott Newman on 11/17/15.
+//  Copyright © 2015 Newman Creative. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
